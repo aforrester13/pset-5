aa1504b37f423125cec282ba183f1fda0d839f0e
Aidan Forrester <AForrester@ucvts.tec.nj.us>
2020-01-06 12:41:42 -0500

    next commit

--- js/shapes.js
+++ js/shapes.js
@@ -21,6 +21,9 @@ window.onload = function() {
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+    document.getElementById("triangle").onclick = drawTriangle;
+    document.getElementById("smile").onclick = drawFace;
+    document.getElementById("pyramid").onclick = drawPyramid;
 }
 
 /*
@@ -49,8 +52,8 @@ window.onload = function() {
 
  const drawRectangle = function() {
    const canvas = document.getElementById('student-canvas-2');
-   const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+   const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
   do {
     var width = prompt("Width: ")
     if (width == null) {
@@ -85,8 +88,8 @@ window.onload = function() {
     }
   } while (width > canvas.width || width < 1 || height > canvas.height || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y))
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeRect(x, y, width, height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.strokeRect(x, y, width, height);
 };
 
 /*
@@ -95,8 +98,8 @@ window.onload = function() {
 
  const drawColoredRectangle = function() {
    const canvas = document.getElementById('student-canvas-3');
-   const ctx = canvas.getContext('2d');
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   const context = canvas.getContext('2d');
+   context.clearRect(0, 0, canvas.width, canvas.height);
    do {
      var color = (prompt("Color: "))
      if (color == null) {
@@ -110,8 +113,8 @@ window.onload = function() {
    } while (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow")
 
    if (color != null) {
-     ctx.fillStyle = color_case;
-     ctx.fillRect(10, 10, 100, 50);
+     context.fillStyle = color_case;
+     context.fillRect(10, 10, 100, 50);
    }
 
  };
@@ -121,7 +124,45 @@ window.onload = function() {
  */
 
 const drawTriangle = function() {
-    // write your exercise 4 code here
+  const canvas = document.getElementById("student-canvas-4")
+  const context = canvas.getContext('2d')
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+  do {
+    var side1 = prompt("Side 1: ")
+    if (side1 == null) {
+      break;
+    }
+
+    var side2 = prompt("Side 2: ")
+    if (side2 == null) {
+      break;
+    }
+
+    var side3 = prompt("Side 3: ")
+    if (side3 == null) {
+      break;
+    }
+
+    var height = Math.min(side1, side2, side3)
+    var hypotenuse = Math.max(side2, side2, side3)
+    var base = Math.sqrt(hypotenuse*hypotenuse - height*height)
+
+    if ((height*height) + (base*base) != (hypotenuse*hypotenuse)) {
+      alert("This is not a valid right triangle.")
+    }
+  } while ((height*height) + (base*base) != (hypotenuse*hypotenuse))
+
+if ((height*height) + (base*base) == (hypotenuse*hypotenuse)) {
+    height = height + 25
+    base = base + 25
+    context.beginPath();
+    context.moveTo(25, 25);
+    context.lineTo(25, height);
+    context.lineTo(base, height);
+    context.lineTo(25, 25);
+    context.stroke();
+  }
 };
 
 /*
@@ -129,7 +170,50 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    // write your exercise 4 code here
+  const canvas = document.getElementById("student-canvas-5")
+  const context = canvas.getContext('2d')
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+  do {
+    var faceRadius = prompt("Radius: ")
+    if (faceRadius == null) {
+      break;
+    }
+
+    if (faceRadius < 32) {
+      alert("Your radius must be at least 32.")
+    }
+
+    if (isNaN(faceRadius)) {
+      alert("Your radius is not a number.")
+    }
+
+    if (faceRadius > 250) {
+      alert("Your smiley face won't fit on the canvas.")
+    }
+  } while (faceRadius < 32 || isNaN(faceRadius) || faceRadius > 250)
+
+  var eyeRadius = faceRadius * .15
+  var mouthRadius = faceRadius * .7
+
+
+
+  context.beginPath();
+  context.arc(512, 256, faceRadius, 0, 2 * Math.PI)
+  context.stroke();
+  context.closePath();
+  context.beginPath();
+  context.arc(512, 256, mouthRadius, 0, Math.PI);
+  context.stroke();
+  context.closePath();
+  context.beginPath();
+  context.arc(512 - 0.4 * faceRadius, 256 - 0.4 * faceRadius, eyeRadius, 0, 2 * Math.PI);
+  context.stroke();
+  context.closePath();
+  context.beginPath();
+  context.arc(512 + 0.4 * faceRadius, 256 - 0.4* faceRadius, eyeRadius, 0, 2 * Math.PI);
+  context.stroke();
+  context.closePath();
 };
 
 /*
